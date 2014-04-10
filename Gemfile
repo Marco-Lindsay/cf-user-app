e0b824dba34f11e0013e5e4fe80b5ce23f4fcff0
marco-lindsay <lindsay_marco@hotmail.com>
2014-04-10 12:15:51 -0700

    baseline new app for codefellows challenge questions 3

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :production do
+	gem 'pg'
+
+end
+
+group :development, :test do
+	gem "better_errors"
+	gem 'quiet_assets'
+	gem 'rails_layout'
+	gem 'sqlite3'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Userapp::Application.config.secret_key_base = '625ce770f7c3949d988b45db126233d648f7375022e1cf2a437e2ce5297c25492ade881bb35ee171ada8f791b70386ff341444ce8dfbb4d3e757cd399016dfcc'
