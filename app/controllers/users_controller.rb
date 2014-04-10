2e5200882bc291c7374058a9aa760d264dbe9dd2
marco-lindsay <lindsay_marco@hotmail.com>
2014-04-10 13:56:44 -0700

    added some bootstrap styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # layout 'bootstrap'
+  layout 'bootstrap'
   
   # GET /users
   # GET /users.json
